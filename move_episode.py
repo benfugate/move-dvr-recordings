e2cd67c847d977424629c9fb728646183fdee81d
benfugate <benjamin_fugate13@yahoo.com>
2022-08-23 16:00:26 -0400

    delete empty folders when done

--- move_episode.py
+++ move_episode.py
@@ -1,19 +1,29 @@
 #!/usr/local/bin/python
 
-from glob import glob
-import shutil
+import os
 import re
+import shutil
+from glob import glob
 
-watch = glob("/watch")
+watch = glob("/watch/**/*", recursive=True)
 for file in watch:
-    if file.endswith((".mkv", "mp4")):
+    filename = os.path.basename(file)
+    if filename.endswith((".mkv", "mp4")):
         try:
-            parts = re.match(r"(.*S\d+E)(\d+)(.*)", file)
+            parts = re.match(r"(.*S\d+E)(\d+)(.*)", filename)
             episode_number = int(parts.group(2))
             new_episode_number = f"{(episode_number + 1):02d}"
             new_filename = parts.group(1) + new_episode_number + parts.group(3)
-            print(f"Changing {file} to {new_filename}...")
-            shutil.copyfile(f"/watch/{file}", f"/dest/{new_filename}")
+            print(f"Changing {filename} to {new_filename}... ", end="")
+            shutil.move(file, f"/dest/{new_filename}")
             print("Success!")
         except Exception as e:
             print(f"Error: {e}")
+
+for folder, _, _ in os.walk("/watch", topdown=False):  # Listing the files
+    if folder == "/watch":
+        break
+    try:
+        os.rmdir(folder)
+    except OSError as ex:
+        continue
