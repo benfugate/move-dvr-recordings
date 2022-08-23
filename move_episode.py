c8fab1d723d8590da3fd68fd54c104e3e680e51f
benfugate <benjamin_fugate13@yahoo.com>
2022-08-23 11:55:04 -0400

    add exceptions

--- move_episode.py
+++ move_episode.py
@@ -7,9 +7,13 @@ import re
 watch = glob("/watch")
 for file in watch:
     if file.endswith((".mkv", "mp4")):
-        parts = re.match(r"(.*S\d+E)(\d+)(.*)", file)
-        episode_number = int(parts.group(2))
-        new_episode_number = f"{(episode_number + 1):02d}"
-        new_filename = parts.group(1) + new_episode_number + parts.group(3)
-        print(f"Changing {file} to {new_filename}")
-        shutil.copyfile(f"/watch/{file}", f"/dest/{new_filename}")
+        try:
+            parts = re.match(r"(.*S\d+E)(\d+)(.*)", file)
+            episode_number = int(parts.group(2))
+            new_episode_number = f"{(episode_number + 1):02d}"
+            new_filename = parts.group(1) + new_episode_number + parts.group(3)
+            print(f"Changing {file} to {new_filename}...")
+            shutil.copyfile(f"/watch/{file}", f"/dest/{new_filename}")
+            print("Success!")
+        except Exception as e:
+            print(f"Error: {e}")
